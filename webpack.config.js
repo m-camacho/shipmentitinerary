7451da95fe3ea6ecd650e1ce36b9d9422bb5e80e
Mario Camacho <mario.camacho@lime-energy.com>
2019-07-07 12:46:13 -0400

    Initial webpack, babel, react configuration

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'index_bundle.js'
+    },
+    resolve: {
+        extensions: [".jsx", ".js"],
+    },
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: [ 'babel-loader' ]
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+};
